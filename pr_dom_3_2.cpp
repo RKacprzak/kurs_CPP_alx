131dd2c10048f52c38e4fd5b753af94850846591
RKacprzak <117547559+RKacprzak@users.noreply.github.com>
2023-01-19 00:44:20 +0100

    Add files via upload

--- pr_dom_3_2.cpp
+++ pr_dom_3_2.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
 
-void szyfruj(char* napis,int p,int k){
 
-    char alfabet [26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W', 'X','Y','Z'};
-    
- for(int i=0;i<k;i++){ 
-    for(int j=0; j<27;j++){
+void szyfruj(std::string &wyraz,int &p){ //argumenty są przekazywane funkcji przez referencę
+
+        // tutaj przypisuję znaki stringa tabeli char (dla przykładu metody 
+        //- przesunięcie można zrobić od razu na pozycjach liter w stringach);
+    std::cout<<"Liczba znaków :"<<wyraz.size()<<"\n"; 
         
-        if(alfabet[j]==napis[i]){
-            napis[i]=alfabet[j+p];
-            break;
-        }
+    int n = wyraz.size();
+        
+    char wyr_tab[n+1]; //n+1 jest podyktowane tym, że zmienna char jest tablicą większą o 1 
+                       //niż liczba znaków, gdyż na końcu ciągu znaków jest zawsze niewidzialne "0", 
+                       //np. napis "Ola" w zapisie char będzie wyglądał jako tabela znaków {O,l,a,0}
+       
+       for(int i=0; i<n+1; i++){
+            
+            wyr_tab[i] = wyraz[i];
+       }
+       
+    char alfabet [52]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W', 'X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W', 'X','Y','Z'};  
+    //najprostszy sposób na zapewnienie ciągłości przesunięć szyfrujących w przypadku ostatnich liter alfabetu 
+    //to drugi zestaw liter alfabetu we wzorcowej tabeli szyfrowej
+    
+        for(int i=0;i<n;i++){ 
+            for(int j=0; j<27;j++){
+        
+                if(alfabet[j]==wyr_tab[i]){
+                    wyr_tab[i]=alfabet[j+p];
+                    break;                    //musimy przerwać działanie tej wewn. pętli w przypadku równości 
+                                             //i przypisania przesuniętej litery alfabetu, gdyż w przeciwnym przypadku równości 
+                                             //i przypisania będą się powtarzały, 
+                                             //bo alfabet[j+p] będzie w następnym kroku pętli literą alfabet [j];
+                    }
+                }
+       std::cout<<wyr_tab[i];   
     }
-       std::cout<<napis[i];    
- }
     std::cout<<"\n";
     
 }
@@ -21,15 +42,12 @@ void szyfruj(char* napis,int p,int k){
 int main(){
     
     int p;
-    int k;
-    std::cout<<"Podaj długość szyfrowanego napisu (liczbę liter):\n";
-    std::cin>>k;
-    char napis[k-1];
+    std::string wyraz;
+    std::cout<<"Podaj szyfrowany wyraz(wielkimi literami): \n";
+    std::cin>>wyraz;
     std::cout<<"Podaj przesunięcie szyfrujące:\n";
     std::cin>>p;
-    std::cout<<"Wprowadź szyfrowany napis: \n";
-    std::cin>>napis;
     std::cout<<"Zaszyfrowany napis:\n";
-    szyfruj(napis,p,k);
+    szyfruj(wyraz,p);
 
 }
