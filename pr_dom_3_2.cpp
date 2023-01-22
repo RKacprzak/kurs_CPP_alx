ed721f66fa428cb939ca20a9a9deda6d589b601e
RKacprzak <117547559+RKacprzak@users.noreply.github.com>
2023-01-22 13:33:05 +0100

    Add files via upload

--- pr_dom_3_2.cpp
+++ pr_dom_3_2.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
 
+std::string wielkie_litery(std::string &wyraz){ 
+    
+      int n = wyraz.size();  
+        for (int i=0; i<n; i++){
+           wyraz[i]=toupper(wyraz[i]);
+        }
+     return wyraz;   
+}
 
-void szyfruj(std::string &wyraz,int &p){ //argumenty są przekazywane funkcji przez referencję
 
-        // tutaj przypisuję znaki stringa tabeli char (dla przykładu metody 
-        //- przesunięcie można zrobić od razu na pozycjach liter w stringach);
-    std::cout<<"Liczba znaków :"<<wyraz.size()<<"\n"; 
-        
+bool czy_litera (std::string &wyraz){  
+    bool y=true;
+    bool x;
     int n = wyraz.size();
-        
-    char wyr_tab[n+1]; //n+1 jest podyktowane tym, że zmienna char jest tablicą większą o 1 
-                       //niż liczba znaków, gdyż na końcu ciągu znaków jest zawsze niewidzialne "0", 
-                       //np. napis "Ola" w zapisie char będzie wyglądał jako tabela znaków {O,l,a,0}
+ 
+    for(int i=0; i<n; i++){
+       
+        if ((wyraz[i] >='a'&& wyraz[i]<='z')||(wyraz[i]>='A' && wyraz[i]<='Z')){
+            x=true;
+        }
+        else{
+            x=false;
+        }
+    y=y*x;     
+   }
+   return y;
+}
+
+void szyfruj(std::string &wyraz,int &p){ 
+
+    std::cout<<"Liczba znaków :"<<wyraz.size()<<"\n"; 
+    int n = wyraz.size();
+    char wyr_tab[n+1];
        
        for(int i=0; i<n+1; i++){
             
@@ -19,18 +40,14 @@ void szyfruj(std::string &wyraz,int &p){ //argumenty są przekazywane funkcji pr
        }
        
     char alfabet [52]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W', 'X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W', 'X','Y','Z'};  
-    //najprostszy sposób na zapewnienie ciągłości przesunięć szyfrujących w przypadku ostatnich liter alfabetu 
-    //to drugi zestaw liter alfabetu we wzorcowej tabeli szyfrowej
+ 
     
         for(int i=0;i<n;i++){ 
             for(int j=0; j<27;j++){
         
                 if(alfabet[j]==wyr_tab[i]){
                     wyr_tab[i]=alfabet[j+p];
-                    break;                    //musimy przerwać działanie tej wewn. pętli w przypadku równości 
-                                             //i przypisania przesuniętej litery alfabetu, gdyż w przeciwnym wypadku równości 
-                                             //i przypisania będą się powtarzały, 
-                                             //bo alfabet[j+p] będzie w następnym kroku pętli literą alfabet [j];
+                    break;                  
                     }
                 }
        std::cout<<wyr_tab[i];   
@@ -45,25 +62,31 @@ int main(){
     std::string wyraz;
     int odp;
    do {
-    std::cout<<"Podaj szyfrowany wyraz(wielkimi literami): \n";
-    std::cin>>wyraz;
-    std::cout<<"Podaj przesunięcie szyfrujące:\n";
-    std::cin>>p;
-    std::cout<<"Zaszyfrowany napis:\n";
-    szyfruj(wyraz,p);
-    odp=0;
+        Restart_1:   
+        std::cout<<"Podaj szyfrowany wyraz: \n";
+        std::cin>>wyraz;
+           
+            if (czy_litera(wyraz)!=1){       
+                std::cout<<"Nie wszystkie znaki są literami. Wprowadź poprawnie napis:\n";
+                goto Restart_1;
+                }
+                
+        wyraz=wielkie_litery(wyraz);
+        std::cout<<"Podaj przesunięcie szyfrujące:\n";
+        std::cin>>p;
+        std::cout<<"Zaszyfrowany napis:\n";
+        szyfruj(wyraz,p);
+        odp=0;
    
-    Restart:   //Etykieta lokalizująca linię kodu
+            Restart_2:   
             std::cout<<"Czy chcesz szyfrować kolejny wyraz? (1-TAK;2-NIE)\n";
         
             std::cin>>odp;
             if(odp!=1 && odp!=2 ) {
                 std::cout<<"Wprowadź poprawną odpowiedź \n";
-                std::cin.clear();              // czyszczenie std::cin ze stanu "FAIL" 
-                                              //po wprowadzeniu zmiennej innego typu, bez tego
-                                             //wywołujemy nieskończoną pętlę przez goto lub alternatywnie przez pętlę while
+                std::cin.clear();      
                 std::cin.ignore(1000,'\n');
-                goto Restart;
+                goto Restart_2;
             }
             
    }  while (odp==1);
